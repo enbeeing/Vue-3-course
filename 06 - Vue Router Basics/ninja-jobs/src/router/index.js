8a2e614716f39c871c5976624d96aec43fbf618e
enbeeing <eddykjllr@hotmail.com>
2022-06-01 13:53:44 +0200

    ninja-jobs done: 404, redirects and lazy loading

--- 06 - Vue Router Basics/ninja-jobs/src/router/index.js
+++ 06 - Vue Router Basics/ninja-jobs/src/router/index.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+// import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
-
+import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/404'
 
 const routes = [
   {
@@ -13,12 +14,30 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: About
+    // lazy loading
+    component: () => import('../views/About.vue')
   },
   {
     path: '/jobs',
     name: 'Jobs',
     component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // 404
+  {
+    path: '/:catchAll(.*)',
+    name: '404',
+    component: NotFound
   }
 ]
 
--- 06 - Vue Router Basics/ninja-jobs/src/views/jobs/Jobs.vue
+++ 06 - Vue Router Basics/ninja-jobs/src/views/jobs/Jobs.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <h2>{{ job.title }}</h2>
+  <div v-for="job in jobs" :key="job.id" class="job">
+    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+      <h2>{{ job.title }}</h2>
+    </router-link>
   </div>
 </template>
 
@@ -11,8 +13,8 @@ export default {
     return {
       jobs: [
         { title: "Ninja UX Designer", id: 1, details: "lorem" },
-        { title: "Ninja Web Developer", id: 1, details: "lorem" },
-        { title: "Ninja Vue Developer", id: 1, details: "lorem" },
+        { title: "Ninja Web Developer", id: 2, details: "lorem" },
+        { title: "Ninja Vue Developer", id: 3, details: "lorem" },
       ],
     };
   },
@@ -20,4 +22,19 @@ export default {
 </script>
 
 <style>
+.job h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
+}
+.job h2:hover {
+  background: #ddd;
+}
+.job a {
+  text-decoration: none;
+}
 </style>
